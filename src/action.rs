eb4c531b100fed07e57b15e4bdd6bd44d3fe1ef4
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-01-28 17:04:43 +0300

    Add todo for action implement

--- src/action.rs
+++ src/action.rs
@@ -1,102 +1,28 @@
-use std::fmt;
+//TODO: implement action type
 
-use crate::project::Project;
-use once_cell::sync::Lazy;
+// use crate::project::Project;
 
-pub trait Action {
-  fn run(&self, project: &Project) -> Result<(), Box<dyn std::error::Error>>;
-}
-
-pub enum ActionType {
-  GitSync,
-  CustomCommand,
-}
-
-impl fmt::Display for ActionType {
-  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    match self {
-      ActionType::GitSync => write!(f, "Git Sync"),
-      ActionType::CustomCommand => write!(f, "Custom Command"),
-    }
-  }
-}
-
-impl ActionType {
-  pub fn iter() -> std::slice::Iter<'static, ActionType> {
-    static ACTIONS: [ActionType; 2] = [ActionType::GitSync, ActionType::CustomCommand];
-    ACTIONS.iter()
-  }
-}
-
-pub struct ActionFactory;
-
-impl ActionFactory {
-  pub fn get_action(action_type: &ActionType) -> Box<dyn Action> {
-    match action_type {
-      ActionType::GitSync => Box::new(GitCommand {
-        git_command_type: GitCommandType::GitSync {
-          remote: lazy_remote(),
-          branch: lazy_branch(),
-        },
-      }),
-      ActionType::CustomCommand => Box::new(CustomCommand {}),
-    }
-  }
-}
-
-// Use once_cell to store values lazily
-static REMOTE: Lazy<String> =
-  Lazy::new(|| inquire::Text::new("Enter the remote: ").prompt().unwrap());
-static BRANCH: Lazy<String> =
-  Lazy::new(|| inquire::Text::new("Enter the branch: ").prompt().unwrap());
-
-fn lazy_remote() -> String {
-  REMOTE.clone()
-}
-
-fn lazy_branch() -> String {
-  BRANCH.clone()
-}
-
-//TODO: implement git commands with git2 crate
-// trait GitCommandTrait {
-//   fn pull(&self, project: &Project) -> Result<(), Box<dyn std::error::Error>>;
-//   fn push(&self, project: &Project) -> Result<(), Box<dyn std::error::Error>>;
-//   fn commit(&self, project: &Project) -> Result<(), Box<dyn std::error::Error>>;
+// #[derive(Debug)]
+// pub enum ActionType {
+//   GitSync { origin: String, branch: String },
+//   CustomCommand { command: String },
 // }
 
-enum GitCommandType {
-  GitSync { remote: String, branch: String },
-}
+// impl ActionType {
+//   fn execute(&self, project: &Project) -> Result<(), Box<dyn std::error::Error>> {
+//     match self {
+//       ActionType::GitSync { origin, branch } => {
+//         println!("{:?} {:?}", origin, branch);
+//         println!("git sync");
+//         println!("project name: {:?}\n", project);
+//       }
+//       ActionType::CustomCommand { command } => {
+//         println!("{:?}", command);
+//         println!("custom command");
+//         println!("project name: {:?}\n", project);
+//       }
+//     }
 
-struct GitCommand {
-  git_command_type: GitCommandType,
-}
-
-impl Action for GitCommand {
-  fn run(&self, project: &Project) -> Result<(), Box<dyn std::error::Error>> {
-    if !project.is_git {
-      return Ok(());
-    }
-
-    match &self.git_command_type {
-      GitCommandType::GitSync { remote, branch } => {
-        println!("{:?} {:?}", remote, branch);
-        println!("git sync");
-        println!("project name: {:?}\n", project);
-      }
-    }
-
-    Ok(())
-  }
-}
-
-struct CustomCommand {}
-
-impl Action for CustomCommand {
-  fn run(&self, project: &Project) -> Result<(), Box<dyn std::error::Error>> {
-    println!("custom command");
-    println!("project name: {:?}\n", project);
-    Ok(())
-  }
-}
+//     Ok(())
+//   }
+// }
