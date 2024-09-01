24876097f3526132612a40efae589923fbd88719
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-09-01 16:41:07 +0300

    Add `actions` module

--- src/action.rs
+++ src/action.rs
@@ -1,28 +1,71 @@
-//TODO: implement action type
+use crate::{errors::ActionError, project::Project};
 
-// use crate::project::Project;
+#[derive(Debug)]
+#[allow(dead_code)]
+pub enum ActionEnum {
+  PULL,
+  PUSH,
+  SYNC,
+}
 
-// #[derive(Debug)]
-// pub enum ActionType {
-//   GitSync { origin: String, branch: String },
-//   CustomCommand { command: String },
-// }
+impl ActionEnum {
+  fn action(&self, project: &Project) -> Result<(), ActionError> {
+    match self {
+      ActionEnum::PULL => {
+        println!("Project: {}", project.name);
+        println!("Executing PULL action...");
+        // Implement the actual pull action logic here
+        Ok(())
+      }
+      ActionEnum::PUSH => {
+        println!("Project: {}", project.name);
+        println!("Executing PUSH action...");
+        // Implement the actual push action logic here
+        Ok(())
+      }
+      ActionEnum::SYNC => {
+        println!("Project: {}", project.name);
+        println!("Executing SYNC action...");
+        // Implement the actual sync action logic here
+        Ok(())
+      }
+    }
+  }
+}
 
-// impl ActionType {
-//   fn execute(&self, project: &Project) -> Result<(), Box<dyn std::error::Error>> {
-//     match self {
-//       ActionType::GitSync { origin, branch } => {
-//         println!("{:?} {:?}", origin, branch);
-//         println!("git sync");
-//         println!("project name: {:?}\n", project);
-//       }
-//       ActionType::CustomCommand { command } => {
-//         println!("{:?}", command);
-//         println!("custom command");
-//         println!("project name: {:?}\n", project);
-//       }
-//     }
+#[allow(dead_code)]
+pub trait ActionTrait {
+  fn run(&self) -> Result<(), ActionError>;
+}
 
-//     Ok(())
-//   }
-// }
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct Action {
+  pub projects: Vec<Project>,
+  pub actions: Vec<String>,
+  pub origin: String,
+  pub branch: String,
+}
+
+#[allow(dead_code)]
+impl ActionTrait for Action {
+  fn run(&self) -> Result<(), ActionError> {
+    for project in &self.projects {
+      if !project.is_git {
+        println!(
+          "{a} project have not a git repository. Skipping...",
+          a = project.name
+        );
+        continue;
+      }
+
+      println!("Executing action for {a} project...", a = project.name);
+
+      ActionEnum::PULL.action(&project);
+      ActionEnum::PUSH.action(&project);
+      ActionEnum::SYNC.action(&project);
+    }
+
+    return Ok(());
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,20 @@ mod answer;
 mod project;
 mod error;
 
+use action::{Action, ActionTrait};
 use answer::Answer;
 use std::process;
 
 fn main() {
   let answers = Answer::build();
+  println!("{:#?}", answers);
 
-  let Answer {
-    projects,
-    actions,
-    origin,
-    branch,
-  } = answers;
+  let action = Action {
+    projects: answers.projects,
+    actions: answers.actions,
+    origin: answers.origin,
+    branch: answers.branch,
+  };
 
   match action.run() {
     Ok(_) => {
